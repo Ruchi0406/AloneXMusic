e2b52f17b69b65cf010c2f78e3aff83baca5336a
AdityaJrsu17 <71754876+Ruchi0406@users.noreply.github.com>
2024-01-01 13:38:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Ruchi0406/AloneXMusic.git",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "aditya")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
